b4b2c4bafeb55917d9bee7305b87ac47381d9f2e
Seulgi Kim <dev@seulgi.kim>
2015-06-20 18:56:47 +0900

    Revert "Seperate gulplint and gulptest."
    This reverts commit 4154013cd261e546438fb6f519e7eb6a53bffc3c.
    
    It was to remove dependency on mocha, istanbul and tslint because sfuture is
    compiled when npm install. But now sfuture is compiled before publishing and
    just publishing JavaScript.
    No need to seperate it.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
 var ts = require('gulp-typescript');
+var tslint = require("gulp-tslint");
 
 gulp.task('build', function () {
   return gulp.src([
@@ -18,11 +21,29 @@ gulp.task('build', function () {
 });
 
 gulp.task("test", ["build"], function (cb) {
-  require('./gulptest')(cb);
+  gulp.src([ './lib/**/*.js' ])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', function () {
+      gulp.src([ './test/**/*.js' ])
+      .pipe(mocha({
+        reporter: 'spec',
+        timeout: 10000
+      }))
+      .pipe(istanbul.writeReports())
+      .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }))
+      .on('end', cb);
+    });
 });
 
 gulp.task("lint", function () {
-  require('./gulplint')();
+  return gulp.src([
+      './**/*.ts',
+      '!./node_modules/**/*.ts',
+      '!./lib.d/**/*.d.ts'
+    ])
+    .pipe(tslint({configuration: require('./.tslintrc')}))
+    .pipe(tslint.report('prose'));
 });
 
 gulp.task("default", ["lint", "test"]);
