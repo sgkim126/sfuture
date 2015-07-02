bcca5408427a6279043b20e8fa7c843a92b4822a
Seulgi Kim <dev@seulgi.kim>
2015-07-03 08:36:06 +0900

    andThen method should keep the value even it throws error.

--- test/andThen.ts
+++ test/andThen.ts
@@ -25,5 +25,38 @@ describe('#andThen', () => {
       done();
     });
   });
-});
 
+  it('should keep the value even it throws error', (done: MochaDone) => {
+    let sequence = 0;
+    let future = Future.successful(100);
+    future.andThen((err: Error, result: number) => {
+      assert.ifError(err);
+      assert.equal(result, 100);
+
+      assert.equal(sequence, 0);
+      sequence += 1;
+
+      throw new Error();
+    }).andThen((err: Error, result: number) => {
+      assert.ifError(err);
+      assert.equal(result, 100);
+
+      assert.equal(sequence, 1);
+      sequence += 10;
+
+      throw new Error();
+    }).andThen((err: Error, result: number) => {
+      assert.ifError(err);
+      assert.equal(result, 100);
+
+      assert.equal(sequence, 11);
+      sequence += 100;
+
+      throw new Error();
+    }).map((result: number) => {
+      assert.equal(result, 100);
+
+      assert.equal(sequence, 111);
+    }).nodify(done);
+  });
+});
