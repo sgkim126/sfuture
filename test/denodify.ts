d4642d61069cbc67864deb6a4870117ebc89da3c
Seulgi Kim <dev@seulgi.kim>
2015-07-03 11:06:14 +0900

    Future.denodify() retruns failed future, if callback throws error.

--- test/denodify.ts
+++ test/denodify.ts
@@ -36,5 +36,16 @@ describe('#denodifiy', () => {
       assert.equal(err.message, 'lhs');
     });
   });
-});
 
+  it('returns failed future, if callback throws error', (done: MochaDone) => {
+    let addPositive = (lhs: number, rhs: number, callback: (err: Error, result?: number) => void) => {
+      throw new Error('throw');
+    };
+
+    let future = Future.denodify(addPositive, null, -100, 100);
+
+    should.fail(future, done, (err) => {
+      assert.equal(err.message, 'throw');
+    });
+  });
+});
